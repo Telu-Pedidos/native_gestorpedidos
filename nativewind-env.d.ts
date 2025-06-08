a8aaacc02e9f59a83df92533faec5a2cee5552a1
jorgeguedess <jorge_guedes00@hotmail.com>
2025-06-08 09:52:58 -0300

    chore: finished setup gluestack

--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="nativewind/types" />
+
+declare module "@env" {
+    export const API_URL: string;
+    export const IMAGEKIT_PUBLIC_KEY: string;
+    export const IMAGEKIT_PRIVATE_KEY: string;
+    export const IMAGEKIT_URL_ENDPOINT: string;
+    export const JWT_SECRET: string;
+  }
+  
